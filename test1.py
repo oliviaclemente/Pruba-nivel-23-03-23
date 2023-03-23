d0d39a3e17612d94d626c30c373564083d956276
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-03-23 09:13:56 +0100

    cc

---
+++ test1.py
@@ -0,0 +1,37 @@
+import math
+math.sqrt(9)
+
+class punto():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+print(punto)
+    
+def cuadrante(self):
+    if self.x>0 and self.y>0:
+        return "El punto {} está en el 1r cuadrante"
+    if self.x<0 and self.y>0:
+        return "El punto {} está en el 2n cuadrante"
+    if self.x<0 and self.y<0:
+        return "El punto {} está en el 3r cuadrante"
+    if self.x>0 and self.y<0:
+        return "El punto {} está en 4t cuadrante"
+print(cuadrante)
+    
+def vector(self, dif_punto):
+    vx= dif_punto.x - self.x
+    vy= dif_punto.y - self.y
+    return "El vector es ({}, {})".format(vx, vy)
+print(vector)
+   
+def distancia(self, dif_punto):
+    distancia= math.sqrt((dif_punto.x - self.x)**2 + (dif_punto.y - self.y)**2)
+    return distancia
+print(distancia)
+
+punto1= punto(2, 3)
+punto2= punto(5, 5)
+punto3= punto(-3, -1)
+punto4= punto(0, 0)
